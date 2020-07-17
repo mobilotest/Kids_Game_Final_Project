74f8b86ba3d39b5c62d978d88d86a762c1cc4565
mobilotest <mobilotest@gmail.com>
2020-07-16 22:21:59 -0700

    working version without some images yet

--- app/src/main/java/com/example/kidsgame/MainActivity.java
+++ app/src/main/java/com/example/kidsgame/MainActivity.java
@@ -1,29 +1,23 @@
 package com.example.kidsgame;
 
 import android.annotation.SuppressLint;
-import android.app.ProgressDialog;
+import android.app.LoaderManager;
 import android.content.Context;
 import android.content.Intent;
-import android.content.res.Resources;
-import android.graphics.Bitmap;
+import android.content.Loader;
 import android.graphics.Color;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.net.Uri;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.text.Editable;
-import android.util.Log;
 import android.view.Gravity;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
-import android.webkit.WebView;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
-import android.widget.ListView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -31,129 +25,25 @@ import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.view.menu.MenuBuilder;
 
-import com.android.volley.Request;
-import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.JsonObjectRequest;
-import com.android.volley.toolbox.StringRequest;
-import com.android.volley.toolbox.Volley;
-import com.example.kidsgame.helper.LocaleHelper;
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-import com.google.gson.JsonSyntaxException;
-import com.kosalgeek.android.json.JsonConverter;
-import com.kosalgeek.asynctask.AsyncResponse;
-import com.kosalgeek.asynctask.PostResponseAsyncTask;
-
-
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.net.URLConnection;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Iterator;
-import java.util.Map;
+import com.squareup.picasso.Picasso;
+import java.util.List;
+import java.util.Random;
 
 import io.paperdb.Paper;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<List<Screen>> {
 
-    String url = "https://github.com/mobilotest/Kids_Game_Final_Project/blob/master/app/src/main/assets/screens.json";
-    ProgressDialog dialog;
+    String url = "https://raw.githubusercontent.com/mobilotest/Kids_Game_Final_Project/master/app/src/main/assets/screens.json";
 
-    String jsonString = "{\n" +
-            "  \"screens\": {\n" +
-            "    \"1\": {\n" +
-            "      \"category\": \"animals\",\n" +
-            "      \"image\": \"https://raw.githubusercontent.com/mobilotest/Kids_Game_Final_Project/master/app/src/main/res/drawable/ant.png\",\n" +
-            "      \"answer\": \"ANT\",\n" +
-            "      \"button_1\": \"DA\",\n" +
-            "      \"button_2\": \"RE\",\n" +
-            "      \"button_3\": \"A\",\n" +
-            "      \"button_4\": \"NO\",\n" +
-            "      \"button_5\": \"T\",\n" +
-            "      \"button_6\": \"BR\",\n" +
-            "      \"button_7\": \"N\",\n" +
-            "      \"button_8\": \"U\"\n" +
-            "    },\n" +
-            "    \"2\": {\n" +
-            "      \"category\": \"animals\",\n" +
-            "      \"image\": \"https://raw.githubusercontent.com/mobilotest/Kids_Game_Final_Project/master/app/src/main/res/drawable/cow.png\",\n" +
-            "      \"answer\": \"COW\",\n" +
-            "      \"button_1\": \"DA\",\n" +
-            "      \"button_2\": \"C\",\n" +
-            "      \"button_3\": \"A\",\n" +
-            "      \"button_4\": \"NY\",\n" +
-            "      \"button_5\": \"T\",\n" +
-            "      \"button_6\": \"O\",\n" +
-            "      \"button_7\": \"SE\",\n" +
-            "      \"button_8\": \"V\"\n" +
-            "    },\n" +
-            "    \"3\": {\n" +
-            "      \"category\": \"animals\",\n" +
-            "      \"image\": \"https://raw.githubusercontent.com/mobilotest/Kids_Game_Final_Project/master/app/src/main/res/drawable/butterfly.png\",\n" +
-            "      \"answer\": \"BUTTERFLY\",\n" +
-            "      \"button_1\": \"ER\",\n" +
-            "      \"button_2\": \"CA\",\n" +
-            "      \"button_3\": \"BA\",\n" +
-            "      \"button_4\": \"ME\",\n" +
-            "      \"button_5\": \"TT\",\n" +
-            "      \"button_6\": \"LY\",\n" +
-            "      \"button_7\": \"N\",\n" +
-            "      \"button_8\": \"F\"\n" +
-            "    },\n" +
-            "    \"4\": {\n" +
-            "      \"category\": \"animals\",\n" +
-            "      \"image\": \"https://raw.githubusercontent.com/mobilotest/Kids_Game_Final_Project/master/app/src/main/res/drawable/cat.png\",\n" +
-            "      \"answer\": \"CAT\",\n" +
-            "      \"button_1\": \"ER\",\n" +
-            "      \"button_2\": \"A\",\n" +
-            "      \"button_3\": \"BA\",\n" +
-            "      \"button_4\": \"ME\",\n" +
-            "      \"button_5\": \"C\",\n" +
-            "      \"button_6\": \"T\",\n" +
-            "      \"button_7\": \"N\",\n" +
-            "      \"button_8\": \"F\"\n" +
-            "    },\n" +
-            "    \"5\": {\n" +
-            "      \"category\": \"transport\",\n" +
-            "      \"image\": \"http://pngimg.com/uploads/tractor/tractor_PNG16116.png\",\n" +
-            "      \"answer\": \"TRACTOR\",\n" +
-            "      \"button_1\": \"R\",\n" +
-            "      \"button_2\": \"RE\",\n" +
-            "      \"button_3\": \"A\",\n" +
-            "      \"button_4\": \"TRA\",\n" +
-            "      \"button_5\": \"C\",\n" +
-            "      \"button_6\": \"BR\",\n" +
-            "      \"button_7\": \"N\",\n" +
-            "      \"button_8\": \"TO\"\n" +
-            "    },\n" +
-            "    \"6\": {\n" +
-            "      \"category\": \"transport\",\n" +
-            "      \"image\": \"http://pngimg.com/uploads/plane/plane_PNG5238.png\",\n" +
-            "      \"answer\": \"PLANE\",\n" +
-            "      \"button_1\": \"X\",\n" +
-            "      \"button_2\": \"RE\",\n" +
-            "      \"button_3\": \"P\",\n" +
-            "      \"button_4\": \"LA\",\n" +
-            "      \"button_5\": \"C\",\n" +
-            "      \"button_6\": \"BR\",\n" +
-            "      \"button_7\": \"NE\",\n" +
-            "      \"button_8\": \"QU\"\n" +
-            "    }\n" +
-            "  }\n" +
-            "}";
+    /**
+     * Constant value for the news loader ID. We can choose any integer.
+     * This really only comes into play if you're using multiple loaders.
+     */
+    private static final int SCREENS_LOADER_ID = 1;
 
-    String answer = "";
+    Screen currentScreen;
+
+    private List<Screen> screens;
 
     private TextView about;
     private String category;
@@ -168,31 +58,39 @@ public class MainActivity extends AppCompatActivity {
     private Button btn_7;
     private Button btn_8;
 
+    private ProgressBar progressBar;
+
     private Button help;
     private Button clear;
     private Button done;
-
-    @Override
-    protected void attachBaseContext(Context newBase) {
-        super.attachBaseContext(LocaleHelper.onAttach(newBase, "en"));
-    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Init Paper first
-        Paper.init(this);
+        progressBar = (ProgressBar) findViewById(R.id.loading_indicator);
 
-        // Default language is English
-        String language = Paper.book().read("language");
-        if (language == null) {
-            Paper.book().write("language", "en");
+        help = (Button) findViewById(R.id.help);
+        clear = (Button) findViewById(R.id.clear);
+        done = (Button) findViewById(R.id.done);
+
+        ConnectivityManager connMgr = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+
+        if (networkInfo != null && networkInfo.isConnected()) {
+            LoaderManager loaderManager = getLoaderManager();
+
+            loaderManager.initLoader(SCREENS_LOADER_ID, null, this);
+        } else {
+
+            View loadingIndicator = findViewById(R.id.loading_indicator);
+            loadingIndicator.setVisibility(View.GONE);
+            result.setText(R.string.no_internet_connection);
         }
+    }
 
-        updateView((String) Paper.book().read("language"));
-
+    private void fillOutMyScreen() {
         about = (TextView) findViewById(R.id.menu_about);
         result = (TextView) findViewById(R.id.result);
         image = (ImageView) findViewById(R.id.image_main);
@@ -205,52 +103,24 @@ public class MainActivity extends AppCompatActivity {
         btn_7 = (Button) findViewById(R.id.button7);
         btn_8 = (Button) findViewById(R.id.button8);
 
-        help = (Button) findViewById(R.id.help);
-        clear = (Button) findViewById(R.id.clear);
-        done = (Button) findViewById(R.id.done);
+        Random random = new Random();
+        int screenIndex = random.nextInt(screens.size());
 
-        try {
-            JSONObject jObject = new JSONObject(jsonString).getJSONObject("screens");
-            Iterator<String> keys = jObject.keys();
-            while (keys.hasNext()) {
-                String key = keys.next();
-                JSONObject innerJObject = jObject.getJSONObject(key);
-                Iterator<String> innerKeys = innerJObject.keys();
+        currentScreen = screens.get(screenIndex);
 
-                answer = innerJObject.getString("answer");
-                String image = innerJObject.getString("image");
-                String button1 = innerJObject.getString("button_1");
-                String button2 = innerJObject.getString("button_2");
-                String button3 = innerJObject.getString("button_3");
-                String button4 = innerJObject.getString("button_4");
-                String button5 = innerJObject.getString("button_5");
-                String button6 = innerJObject.getString("button_6");
-                String button7 = innerJObject.getString("button_7");
-                String button8 = innerJObject.getString("button_8");
+        btn_1.setText(currentScreen.getButton_1());
+        btn_2.setText(currentScreen.getButton_2());
+        btn_3.setText(currentScreen.getButton_3());
+        btn_4.setText(currentScreen.getButton_4());
+        btn_5.setText(currentScreen.getButton_5());
+        btn_6.setText(currentScreen.getButton_6());
+        btn_7.setText(currentScreen.getButton_7());
+        btn_8.setText(currentScreen.getButton_8());
 
-                btn_1.setText(button1);
-                btn_2.setText(button2);
-                btn_3.setText(button3);
-                btn_4.setText(button4);
-                btn_5.setText(button5);
-                btn_6.setText(button6);
-                btn_7.setText(button7);
-                btn_8.setText(button8);
-            }
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
-    }
-
-    private void updateView(String lang) {
-        Context context = LocaleHelper.setLocale(this, lang);
-        Resources resources = context.getResources();
-//        help.setText(resources.getString(R.string.help));
-//        clear.setText(resources.getString(R.string.clear));
-//        done.setText(resources.getString(R.string.done));
-//        about.setText(resources.getString(R.string.settings_about));
-// Android Studio Tutorial - How to change app language without changing phone language edmt dev:
-//        https://www.youtube.com/watch?v=ywF-ySiBAsc
+        // Get the news image from the json and set this image on the imageView
+        Picasso.get().load(currentScreen.getImage()).into(image);
+        image.setVisibility(View.VISIBLE);
+        result.setText("");
     }
 
     /**
@@ -258,7 +128,7 @@ public class MainActivity extends AppCompatActivity {
      **/
     public void help(View v) {
         EditText result = (EditText) findViewById(R.id.result);
-        result.setText(answer);
+        result.setText(currentScreen.getAnswer());
     }
 
     /**
@@ -272,11 +142,12 @@ public class MainActivity extends AppCompatActivity {
     /**
      * This method will check if answer is correct or not and throw a toast message accordingly
      **/
-    public void done(View view) {
+    public void done(View view) throws InterruptedException {
         EditText edit_text = (EditText) findViewById(R.id.result);
 
-        if (edit_text.getText().toString().equalsIgnoreCase(String.valueOf(answer))) { // HOW TO COMPARE?
+        if (edit_text.getText().toString().equalsIgnoreCase(String.valueOf(currentScreen.getAnswer()))) { // HOW TO COMPARE?
             toastMessage(R.string.great);
+            fillOutMyScreen();
         } else {
             toastMessage(R.string.fail);
         }
@@ -415,16 +286,14 @@ public class MainActivity extends AppCompatActivity {
 
         // Handle presses on the action bar items
         if (item.getItemId() == R.id.flag_us) {
-            Paper.book().write("language", "en");
-            updateView((String) Paper.book().read("language"));
+            // code here
         } else if (item.getItemId() == R.id.flag_su) {
 
             Paper.book().write("language", "ru");
-            updateView((String) Paper.book().read("language"));
+            // code here
         }
         return true;
     }
-
 
     /**
      * This method switch Game between RUS and ENG modes
@@ -432,4 +301,32 @@ public class MainActivity extends AppCompatActivity {
     public void onComposeAction(MenuItem mi) {
         // handle click here
     }
+
+    @Override
+    public Loader<List<Screen>> onCreateLoader(int i, Bundle bundle) {
+
+        Uri baseUri = Uri.parse(url);
+        Uri.Builder uriBuilder = baseUri.buildUpon();
+
+        uriBuilder.scheme("https").authority("content.guardianapis.com");
+        uriBuilder.appendQueryParameter("q", "android");
+        uriBuilder.appendQueryParameter("api-key", "test");
+        uriBuilder.build();
+
+        return new ScreenLoader(this, baseUri.toString());
+    }
+
+    @Override
+    public void onLoadFinished(Loader<List<Screen>> loader, List<Screen> screens) {
+        // Hide loading indicator because the data has been loaded
+        View loadingIndicator = findViewById(R.id.loading_indicator);
+        loadingIndicator.setVisibility(View.GONE);
+        this.screens = screens;
+        fillOutMyScreen();
+    }
+
+    @Override
+    public void onLoaderReset(Loader<List<Screen>> loader) {
+    }
+
 }
---
+++ app/src/main/java/com/example/kidsgame/Screen.java
@@ -0,0 +1,58 @@
+package com.example.kidsgame;
+
+public class Screen {
+    private String image;
+    private String answer;
+    private String button_1;
+    private String button_2;
+    private String button_3;
+    private String button_4;
+    private String button_5;
+    private String button_6;
+    private String button_7;
+    private String button_8;
+
+    public Screen(String image, String answer, String button_1, String button_2, String button_3, String button_4, String button_5, String button_6, String button_7, String button_8) {
+        this.image = image;
+        this.answer = answer;
+        this.button_1 = button_1;
+        this.button_2 = button_2;
+        this.button_3 = button_3;
+        this.button_4 = button_4;
+        this.button_5 = button_5;
+        this.button_6 = button_6;
+        this.button_7 = button_7;
+        this.button_8 = button_8;
+    }
+
+    public String getImage() {
+        return image;
+    }
+    public String getAnswer() {
+        return answer;
+    }
+    public String getButton_1() {
+        return button_1;
+    }
+    public String getButton_2() {
+        return button_2;
+    }
+    public String getButton_3() {
+        return button_3;
+    }
+    public String getButton_4() {
+        return button_4;
+    }
+    public String getButton_5() {
+        return button_5;
+    }
+    public String getButton_6() {
+        return button_6;
+    }
+    public String getButton_7() {
+        return button_7;
+    }
+    public String getButton_8() {
+        return button_8;
+    }
+}
